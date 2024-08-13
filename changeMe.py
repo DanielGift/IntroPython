4eec8b3529bdad2b39b9f0cad87a685730545b28
Daniel Gift <daniel.gift@sbs.org>
2024-08-13 13:51:23 -0400

    Create changeMe.py

---
+++ changeMe.py
@@ -0,0 +1,3 @@
+#This file prints out a greeting.
+#You should change the name below to your own. Be sure to leave the punctuation intact.
+print("Hello! My name is Dr. Gift. It's nice to meet you!!")
